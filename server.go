680a2d50f550270b9120480ef1b2e760aad0c6e8
Joe Schafer <joe@jschaf.com>
2019-02-16 17:56:10 -0800

    clean up code

--- server.go
+++ server.go
@@ -13,8 +13,10 @@ package main
 import (
 	"bufio"
 	"errors"
+	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/textproto"
@@ -23,59 +25,63 @@ import (
 	"syscall"
 )
 
-// netFD is a file descriptor for a socket.
-type netFD struct {
+// netSocket is a file descriptor for a socket.
+type netSocket struct {
 	// System file descriptor.
-	Sysfd int
+	fd int
 }
 
-func (fd netFD) Read(p []byte) (int, error) {
+func (ns netSocket) Read(p []byte) (int, error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
-	n, err := syscall.Read(fd.Sysfd, p)
+	n, err := syscall.Read(ns.fd, p)
 	if err != nil {
 		n = 0
 	}
 	return n, err
 }
 
-func (fd netFD) Write(p []byte) (int, error) {
-	n, err := syscall.Write(fd.Sysfd, p)
+func (ns netSocket) Write(p []byte) (int, error) {
+	n, err := syscall.Write(ns.fd, p)
 	if err != nil {
 		n = 0
 	}
 	return n, err
 }
 
-func (fd *netFD) Accept() (*netFD, error) {
-	nfd, _, err := syscall.Accept(fd.Sysfd)
+func (ns *netSocket) Accept() (*netSocket, error) {
+	// syscall.ForkLock doc states lock not needed for blocking accept.
+	nfd, _, err := syscall.Accept(ns.fd)
 	if err == nil {
 		syscall.CloseOnExec(nfd)
 	}
 	if err != nil {
 		return nil, err
 	}
-	return &netFD{nfd}, nil
+	return &netSocket{nfd}, nil
 }
 
-func (fd *netFD) Close() error {
-	return syscall.Close(fd.Sysfd)
+func (ns *netSocket) Close() error {
+	return syscall.Close(ns.fd)
 }
 
 type responseWriter struct {
-	fd *netFD
+	ns *netSocket
 }
 
 func (w responseWriter) Write(b []byte) (int, error) {
 	log.Print("writing: " + string(b))
-	return (*w.fd).Write(b)
+	return (*w.ns).Write(b)
 }
 
 // Creates a new socket file descriptor, binds it and listens on it.
-func newNetFD(ip net.IP, port int) (*netFD, error) {
-	// socket syscall doesn't block so use ForkLock
+func newNetSocket(ip net.IP, port int) (*netSocket, error) {
+	// ForkLock docs state that socket syscall requires the lock.
 	syscall.ForkLock.Lock()
+	// AF_INET = Address Family for IPv4
+	// SOCK_STREAM = virtual circuit service
+	// 0: the protocol for SOCK_STREAM, there's only 1.
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		return nil, os.NewSyscallError("socket", err)
@@ -101,32 +107,28 @@ func newNetFD(ip net.IP, port int) (*netFD, error) {
 		return nil, os.NewSyscallError("listen", err)
 	}
 
-	return &netFD{Sysfd: fd}, nil
+	return &netSocket{fd: fd}, nil
 }
 
-// The HandlerFunc type is an adapter to allow the use of
-// ordinary functions as HTTP handlers. If f is a function
-// with the appropriate signature, HandlerFunc(f) is a
-// Handler that calls f.
+// Type adapter to allow use of ordinary functions as handlers.
 type handlerFunc func(responseWriter, *request)
 
-var defaultServeMux = make(map[string]handlerFunc)
+type serveMux map[string]handlerFunc
 
-func handle(pattern string, handler handlerFunc) {
-	defaultServeMux[pattern] = handler
+var muxes = make(serveMux)
+
+func (m serveMux) handle(pattern string, handler handlerFunc) {
+	m[pattern] = handler
 }
 
 // Finds the a handler that matches the request path.
 // Picks the longest handler in case of a tie.
-func findHandler(r *request) (handlerFunc, error) {
+func (m serveMux) findHandler(r *request) (handlerFunc, error) {
 	var h handlerFunc = nil
 	var l = 0
-	for k, v := range defaultServeMux {
-		// Bunch of ignored bugs here.
-		//   - prefix and slash semantics
+	for k, v := range m {
 		if strings.HasPrefix(r.uri, k) {
 			log.Printf("Found handler %s that matched uri: %s", k, r.uri)
-
 			if len(k) > l {
 				l = len(k)
 				h = v
@@ -137,6 +139,15 @@ func findHandler(r *request) (handlerFunc, error) {
 		return nil, errors.New("no handler for path: " + r.uri)
 	}
 	return h, nil
+}
+
+func (m serveMux) dispatch(w responseWriter, r *request) error {
+	h, err := m.findHandler(r)
+	if err != nil {
+		return err
+	}
+	h(w, r)
+	return nil
 }
 
 func writeHtml(f func(*request) string) handlerFunc {
@@ -150,7 +161,7 @@ func writeHtml(f func(*request) string) handlerFunc {
 	}
 }
 
-func notFoundHandler(w responseWriter, r *request) {
+func notFound(w responseWriter, r *request) {
 	io.WriteString(w, "HTTP/1.0 404 Not Found\r\n"+
 		"Content-Type: text/plain; charset=utf-8\r\n"+
 		"Content-Length: 0\r\n"+
@@ -158,17 +169,15 @@ func notFoundHandler(w responseWriter, r *request) {
 		"\r\n")
 }
 
-type header map[string][]string
-
 type request struct {
 	method string // GET, POST, etc.
-	header header
+	header textproto.MIMEHeader
 	body   []byte
 	uri    string // The raw URI from the request
 	proto  string // "HTTP/1.1"
 }
 
-func readRequest(c *netFD) (*request, error) {
+func parseRequest(c *netSocket) (*request, error) {
 	b := bufio.NewReader(*c)
 	tp := textproto.NewReader(b)
 	req := new(request)
@@ -178,43 +187,40 @@ func readRequest(c *netFD) (*request, error) {
 	s, _ = tp.ReadLine()
 	sp := strings.Split(s, " ")
 	req.method, req.uri, req.proto = sp[0], sp[1], sp[2]
-	log.Printf("parsed request: method=%s, uri=%s, proto=%s", req.method, req.uri, req.proto)
 
 	// Parse headers
 	mimeHeader, _ := tp.ReadMIMEHeader()
-	req.header = header(mimeHeader)
-	log.Printf("request headers:")
-	for k, vs := range mimeHeader {
-		for _, v := range vs {
-			log.Printf("    %s: %s", k, v)
-		}
-	}
+	req.header = mimeHeader
 
 	// Parse body
 	if req.method == "GET" || req.method == "HEAD" {
 		return req, nil
 	}
-
-	body := make([]byte, 1024)
-	n, _ := b.Read(body)
-	// Rest is the body
-	req.body = body[:n]
-	log.Printf("body: " + string(req.body))
+	body, err := ioutil.ReadAll(b)
+	if err != nil {
+		return nil, err
+	}
+	req.body = body
 	return req, nil
 }
 
 func main() {
+	ipFlag := flag.String("ip_addr", "127.0.0.1", "The IP address to use")
+	portFlag := flag.Int("port", 8080, "The port to use.")
+	flag.Parse()
 
-	handle("/hello", writeHtml(func(_ *request) string { return "<h1>Hello world</h1>" }))
-	handle("/notfound", handlerFunc(notFoundHandler))
-	handle("/", writeHtml(func(r *request) string {
-		return "<h1>Using fallback matcher for path: " + r.uri + "</h1>"
-	}))
+	muxes.handle("/hello",
+		writeHtml(func(_ *request) string { return "<h1>Hello world</h1>" }))
+	muxes.handle("/notfound", handlerFunc(notFound))
+	muxes.handle("/",
+		writeHtml(func(r *request) string {
+			return "<h1>Using fallback matcher for path: " + r.uri + "</h1>"
+		}))
 
-	ip := net.IPv4(127, 0, 0, 1)
-	port := 8080
-	fd, err := newNetFD(ip, port)
-	defer fd.Close()
+	ip := net.ParseIP(*ipFlag)
+	port := *portFlag
+	socket, err := newNetSocket(ip, port)
+	defer socket.Close()
 	if err != nil {
 		panic(err)
 	}
@@ -227,28 +233,28 @@ func main() {
 
 	for {
 		// Block until incoming connection
-		rw, e := fd.Accept()
+		rw, e := socket.Accept()
 		log.Print()
 		log.Print()
-		log.Print("Incoming connection")
+		log.Printf("Incoming connection")
 		if e != nil {
 			panic(e)
 		}
 
 		// Read request
 		log.Print("Reading request")
-		req, err := readRequest(rw)
+		req, err := parseRequest(rw)
+		log.Print("request: ", req)
 		if err != nil {
 			panic(err)
 		}
 
 		// Write response
 		log.Print("Writing response")
-		h, err := findHandler(req)
+		err = muxes.dispatch(responseWriter{rw}, req)
 		if err != nil {
 			log.Print(err.Error())
 			continue
 		}
-		h(responseWriter{rw}, req)
 	}
 }
